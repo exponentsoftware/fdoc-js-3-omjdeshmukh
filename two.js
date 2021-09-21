8e872ba3a5dfe48e9481f8795d06b45d6cb17866
om <72704286+omjdeshmukh@users.noreply.github.com>
2021-09-21 17:36:29 +0530

    Create two.js

---
+++ two.js
@@ -0,0 +1,31 @@
+
+const student = {
+  name: 'David',
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: 'HTML', level: 10 },
+      { skill: 'CSS', level: 8 },
+      { skill: 'JS', level: 8 },
+      { skill: 'React', level: 9 },
+    ],
+    backEnd: [
+      { skill: 'Node', level: 7 },
+      { skill: 'GraphQL', level: 8 },
+    ],
+    dataBase: [{ skill: 'MongoDB', level: 7.5 }],
+    dataScience: ['Python', 'R', 'D3.js'],
+  },
+};
+
+// Add Bootstrap with level 8 to the front end skill sets
+// Add Express with level 9 to the back end skill sets
+// Add SQL with level 8 to the data base skill sets
+// Add SQL without level to the data science skill sets
+
+student.skills.frontEnd[4] = {skill:'Bootstrap',level:8}
+student.skills.backEnd[2] = {skill:'Express',level:9}
+student.skills.dataBase[1] = {skill:'SQL',level:8}
+student.skills.dataScience[3] = 'SQL'
+
+console.log(student)
